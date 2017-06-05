e638fc65aa12bd136594451b8c185a7a01ef3e9a
Johanna Amann <johanna@icir.org>
2017-06-05 12:29:46 -0700

    Updating CHANGES and VERSION.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ py_modules = [
 ]
 
 setup(name='btest',
-      version="0.56-22", # Filled in automatically.
+      version="0.57", # Filled in automatically.
       description='A simple unit testing framework',
       long_description='See https://github.com/bro/btest',
       author='Robin Sommer',
