3dee4d1b732af54ea14d142f350f498c3f35b8c4
Robin Sommer <robin@corelight.com>
2020-01-17 09:09:37 +0000

    Show diagnostics for expected failures when --diagnostics-all is used.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ py_modules = [
 ]
 
 setup(name='btest',
-      version="0.59-12", # Filled in automatically.
+      version="0.6", # Filled in automatically.
       description='A simple unit testing framework',
       long_description='See https://github.com/zeek/btest',
       author='Robin Sommer',
