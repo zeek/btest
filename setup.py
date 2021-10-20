22534d5672f3002baafa7544dcdf269d44b3f184
Christian Kreibich <christian@corelight.com>
2021-10-20 15:35:34 -0700

    Remove Python 2.7 from manifest
    BTest runs as "python3" at this point.

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Utilities',
     ],
