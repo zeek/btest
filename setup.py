5f954ec65cb78b17f7156455c8c3c905a816ae96
Christian Kreibich <christian@corelight.com>
2021-11-01 12:08:19 -0700

    Updating CHANGES and VERSION.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ py_modules = ["btest-sphinx"]
 
 setup(
     name='btest',
-    version="0.70-6",  # Filled in automatically.
+    version="0.71",  # Filled in automatically.
     description='A powerful system testing framework',
     long_description='See https://github.com/zeek/btest',
     author='Robin Sommer',
