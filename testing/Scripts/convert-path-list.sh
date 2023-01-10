f256b5ab0d7bb3007aad99de2c7d8d1fbf6c0afb
Tim Wojtulewicz <timwoj@gmail.com>
2023-01-10 15:54:47 -0700

    Add tests.environment-windows btest
    The original tests.environment btest doesn't work on Windows due to some
    path differences in the output. This adds a new test that does the same
    things except does some additional conversions in the test script itself
    to remove those differences.

---
+++ testing/Scripts/convert-path-list.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script is used by tests.environment-windows to convert a semi-colon
+# separated list of Windows-style paths into a colon-separate list of
+# POSIX-style paths.
+
+new_list=""
+
+IFS=';' read -ra PARTS <<<"$1"
+for i in "${PARTS[@]}"; do
+    p=$(cygpath "${i}" | sed 's/\/$//')
+    new_list+=$p
+    new_list+=":"
+done
+
+echo ${new_list%?}
